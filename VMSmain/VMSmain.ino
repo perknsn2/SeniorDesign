21c128c727cd935a1820b5c54562417354fde232
Ishan Ahuja <iahuja2@illinois.edu>
2016-04-16 21:44:21 -0500

    Implemented Serial.find()

--- VMSmain/VMSmain.ino
+++ VMSmain/VMSmain.ino
@@ -30,18 +30,8 @@ char ip_address[16];
 
 
 //ESP8266 Keywords 
-const char CWMODE = '1';//CWMODE 1=STATION, 2=APMODE, 3=BOTH
 const char CIPMUX = '1';//CWMODE 0=Single Connection, 1=Multiple Connections
-const char keyword_OK[] = "OK";
-const char keyword_ready[] = "ready";
-const char keyword_no_change[] = "no change";
-const char keyword_blank[] = "#&";
-const char keyword_ip[] = "192.";
-const char keyword_rn[] = "\r\n";
-const char keyword_quote[] = "\"";
-const char keyword_carrot[] = ">";
-const char keyword_sendok[] = "SEND OK";
-const char keyword_linkdisc[] = "Unlink";
+
 
 void setup(){
     //Setting up input and output pins
@@ -60,7 +50,7 @@ void setup(){
 
 void loop(){
   ping();
-  delay(3000);
+  delay(7000);
 }
 
 
