3b9b2d3ce1157d03aa1a2f60b9d0402ee329c6c2
sutomi2 <samuelutomi@yahoo.com>
2016-04-17 02:33:33 -0400

    Changed

--- Arduino/Sketch1/FXAS21002.ino
+++ Arduino/Sketch1/FXAS21002.ino
@@ -1,7 +1,5 @@
 #define FXAS21002C_I2C_ADDRESS 0x20
 #define FXAS21002C_CTRL_REG1 0x13
-#define MMA8653FC_I2C_ADDRESS 0x1D   //accelerometer device address
-#define MMA8653FC_CTRL_REG1 0x2A    //accelerometer active mode register 
 
 void init_gyro()
 {
@@ -9,9 +7,4 @@ void init_gyro()
 	write_I2C(FXAS21002C_I2C_ADDRESS, FXAS21002C_CTRL_REG1, &data, 1);
 }
 
-void init_accelerometer()
-{
-  uint8_t data = 0x1;
-  write_I2C(MMA8653FC_I2C_ADDRESS, MMA8653FC_CTRL_REG1, &data, 1);
-  
-}
+
