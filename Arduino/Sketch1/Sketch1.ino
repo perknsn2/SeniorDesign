8ef30af5d5357d732e98a05c09eaf9fdff5b402c
sutomi2 <samuelutomi@yahoo.com>
2016-04-19 15:34:48 -0400

    Commit

--- Arduino/Sketch1/Sketch1.ino
+++ Arduino/Sketch1/Sketch1.ino
@@ -41,12 +41,33 @@ void loop() {
   int n;
   uint8_t data;
 
-  n = read_I2C(FXAS21002C_I2C_ADDRESS, FXAS21002C_WHO_AM_I, &data, 1);
-
-  Serial.print("n:");
-  Serial.println(n);
-
+  n = read_I2C(FXAS21002C_I2C_ADDRESS, FXAS21002C_WHO_AM_I, &data, 1);    
+  Serial.print("Gyro Device ID: ");
   Serial.println(data, HEX);
+
+  
+
+  n = read_I2C(MMA8653FC_I2C_ADDRESS,MMA8653FC_WHO_AM_I,&data,1);
+  Serial.print("Accelerometer Device ID: ");
+  Serial.println(data, HEX);
+
+  uint8_t buffer[4];
+  uint16_t x;
+  uint16_t y;
+  double accel_x;
+  double accel_y;
+  read_I2C(MMA8653FC_I2C_ADDRESS, X_MSB, buffer, 4);
+  x =((buffer[0]<<8) | buffer[1]) >> 6;
+  accel_x = (double(x)/511)*1.996;
+  y =((buffer[2]<<8) | buffer[3]) >> 6;
+  accel_y = (double(y)/511)*1.996;  
+  Serial.print("X-axis acceleration: ");
+  Serial.print(accel_x);
+  Serial.println("g");
+  Serial.print("Y-axis acceleration: ");
+  Serial.print(accel_y);
+  Serial.println("g");
+  
   delay(500);
 }
 
